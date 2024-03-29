b976c20dc7979b326fee3c59014ba382c5af9542
Shiloh George <shilohgeorge2019@gmail.com>
2024-03-29 14:54:08 +0100

    formated footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html lang='en'>
-			<body className={`${inter.className} text-black text-sm font-normal font-sans`}>
+			<body className={`${inter.className} text-gray-500 text-sm font-normal font-sans`}>
 				{children}
 				<Toaster
 					richColors
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,22 +1,45 @@
 import Link from 'next/link';
+import { AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import { FaFacebookF } from 'react-icons/fa';
 
 export function Footer() {
+	const h3Classess = 'font-bold text-md text-gray-600';
+	const socialLInksAnimations = 'hover:scale-110 transition duration-500 ease-in-out';
 	return (
-		<footer className={`w-full min-h-40 bg-gray-300 grid grid-cols-3 p-12 text-xs`}>
+		<footer className={`w-full min-h-40 bg-gray-200 grid grid-cols-3 p-12 text-xs`}>
 			<div className='flex flex-col gap-1'>
-				<h3 className='font-bold text-md'>About </h3>
-				<nav className='pl-2 space-y-2'>
+				<h3 className={h3Classess}>About </h3>
+				<nav className='space-y-2'>
 					<Link
-						href={'/'}
-						className=''>
-						CAS - Classroom Allocation Management System
+						href={'/aboutus'}
+						className='hover:font-medium transition duration-500 hover:underline ease-in-out'>
+						About CAS - Classroom Allocation System
 					</Link>
 					<p className=''>
-						<span className='font-bold text-md'>&copy;</span> 2024 Classroom Allocation Management System
+						<span className='font-bold'>&copy;</span> 2024 Classroom Allocation Management System
 					</p>
 				</nav>
 			</div>
-			<h3 className=''>Follow Us</h3>
+			<div className='flex flex-col gap-1'>
+				<h3 className={h3Classess}>Follow Us</h3>
+				<nav className='flex gap-x-3 text-2xl text-black'>
+					<Link
+						href={'/#instagram'}
+						className={socialLInksAnimations}>
+						<AiOutlineInstagram />
+					</Link>
+					<Link
+						href={'/#twitter'}
+						className={socialLInksAnimations}>
+						<AiOutlineTwitter />
+					</Link>
+					<Link
+						href={'/#facebook'}
+						className={socialLInksAnimations}>
+						<FaFacebookF />
+					</Link>
+				</nav>
+			</div>
 		</footer>
 	);
 }
