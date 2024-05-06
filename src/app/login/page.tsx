7769bf214287b41eb6c2ec76c31b6c21d11bad04
Shiloh George <shilohgeorge2019@gmail.com>
2024-05-06 18:30:20 +0100

    added back button to login page and made ui improvement

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,8 @@
+import Link from 'next/link';
 import { Metadata } from 'next';
 import { LoginClient } from './loginClient';
 import { FaLaptop } from 'react-icons/fa6';
+import { FaArrowLeft } from 'react-icons/fa';
 
 export const metadata: Metadata = {
 	title: 'login',
@@ -10,8 +12,14 @@ export const metadata: Metadata = {
 export default function Login() {
 	return (
 		<main className={`w-full h-screen flex flex-col p-4 items-center justify-center bg-gray-200`}>
-			<section className='flex flex-col w-[95%] md:w-1/2 gap-10 md:gap-1 bg-white min-h-[85%] rounded-2xl items-center justify-center'>
-				<div className='w-full flex items-end justify-center text-3xl font-semibold min-h-[5vh] md:min-h-[13vh]'>
+			<section className='flex flex-col w-[95%] md:w-1/2 gap-10 md:gap-1 bg-white min-h-[60%] md:min-h-[70%] rounded-2xl items-center justify-center relative'>
+				<Link
+					title={`back button`}
+					className={`anchor flex items-center justify-center absolute top-2 left-2 md:top-4 md:left-4 bg-gray-200 hover:bg-gray-500 text-gray-500 hover:text-white rounded-xl transition ease-linear duration-500 text-base size-8`}
+					href={`/`}>
+					<FaArrowLeft />
+				</Link>
+				<div className='w-full flex items-end justify-center text-3xl font-semibold '>
 					<h2 className='flex items-center justify-center gap-2'>
 						<FaLaptop />
 						<span className='tracking-widest'>CAS</span>
